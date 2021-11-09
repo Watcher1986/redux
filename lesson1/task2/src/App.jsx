36a5ce38d860ce61b5b364ad03625454b8dde4f4
Denis <Deniss2007@gmail.com>
2021-11-09 15:53:20 +0200

    default context

---
+++ lesson1/task2/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { DefaultContext } from "./default-context";
+import Header from "./Header";
+
+class App extends Component {
+  state = {
+    userdata: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <DefaultContext.Provider value={this.state.userdata}>
+          <Header />
+        </DefaultContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lesson1/task2/src/UserMenu.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { DefaultContext } from './default-context';
+
+class UserMenu extends Component {
+  render() {
+    const { name, avatar_url } = this.context
+    return (
+      <div className="menu">
+        <span className="menu__greeting">{`Hello, ${name}`}</span>
+        <img
+          alt="User Avatar"
+          src={avatar_url}
+          className="menu__avatar"
+        />
+      </div>
+    );
+  }
+}
+
+UserMenu.contextType = DefaultContext;
+
+export default UserMenu;
