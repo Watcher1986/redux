56f0ebfa5345b62f4e1f2747d3ddbb84d4afc0bd
Denis <Deniss2007@gmail.com>
2021-11-10 21:14:26 +0200

    redux actions , store

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -1,32 +1,32 @@
 import { createStore } from 'redux';
 
-// const INCREMENT = 'COUNTER/INCREMENT';
-// const DECREMENT = 'COUNTER/DECREMENT';
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
 
 export const actionCreatorIncrement = {
-  type: 'COUNTER/INCREMENT',
+  type: INCREMENT,
 };
 
 export const actionCreatorDecrement = {
-  type: 'COUNTER/DECREMENT',
+  type: DECREMENT,
 };
 
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
-    case 'COUNTER/INCREMENT':
+    case INCREMENT:
       return state + 1;
-    case 'COUNTER/DECREMENT':
+    case DECREMENT:
       return state - 1;
     default:
       return state;
   }
 };
 
-const store = createStore(counterReducer);
+export const store = createStore(counterReducer);
 
-// store.dispatch(incrementActionCreator());
-// store.dispatch(incrementActionCreator());
-// store.dispatch(incrementActionCreator());
-//store.dispatch(decrementActionCreator());
+store.dispatch(actionCreatorIncrement());
+store.dispatch(actionCreatorIncrement());
+store.dispatch(actionCreatorIncrement());
+store.dispatch(actionCreatorDecrement());
 
 // console.log(store.getState());
