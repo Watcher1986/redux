d0ee12f34d190edb6f47e817ebd454749c123c0b
Denis <Deniss2007@gmail.com>
2021-11-18 16:06:13 +0200

    df

--- lesson2/task4/src/users.reducer.js
+++ lesson2/task4/src/users.reducer.js
@@ -1,17 +1,18 @@
 import { addUser, deleteUser } from './users.actions';
 
 const reducer = (state, action) => {
-  const someData = action.payload;
+  const addUserAction = addUser();
+  const delUserAction = deleteUser();
   switch (action.type) {
-    case addUser(someData !== ''):
+    case addUserAction.type === 'USERS/ADDUSER':
       return {
         ...state,
-        usersList: [...state.usersList, state.usersList.concat(someData)],
+        usersList: [...state.usersList, state.usersList.concat(addUserAction.payload)],
       };
-    case deleteUser(someData !== 0):
+    case delUserAction.type === 'USERS/REMOVEUSER':
       return {
         ...state,
-        usersList: [...state.usersList, state.usersList.filter(user => user.id !== someData)],
+        usersList: [...state.usersList, state.usersList.filter(user => user.id !== delUserAction.payload)],
       };
     default:
       return state;
