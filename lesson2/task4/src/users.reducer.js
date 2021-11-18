89bbdeeaff3295c8349262b4a217a670461e82a0
Denis <Deniss2007@gmail.com>
2021-11-18 17:51:42 +0200

    ывффыв

--- lesson2/task4/src/users.reducer.js
+++ lesson2/task4/src/users.reducer.js
@@ -3,14 +3,14 @@ import { addUser, ADDUSER, deleteUser, DELETEUSER } from './users.actions';
 const reducer = (state, action) => {
   switch (action.type) {
     case ADDUSER: {
-      const res = () => addUser()
+      const res = addUser();
       return {
         ...state,
         usersList: [...state.usersList, state.usersList.concat(res.payload)],
       };
     }
     case DELETEUSER: {
-      const res = () => deleteUser()
+      const res = deleteUser();
       return {
         ...state,
         usersList: state.usersList.filter(user => user.id !== res.payload),
