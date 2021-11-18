1a40a527402325463563a78246bf8230d8bc0e0b
Denis <Deniss2007@gmail.com>
2021-11-18 18:19:07 +0200

    fsd

--- lesson2/task4/src/store.js
+++ lesson2/task4/src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux';
-import reducer from './users.reducer';
+import { usersReducer } from './users.reducer';
 
 const initialState = { usersList: [] }
 
-const store = createStore(reducer, initialState);
+const store = createStore(usersReducer, initialState);
 
 export default store;
