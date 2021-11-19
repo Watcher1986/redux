30f27cba59824e893dce051551ebf7505f90e212
Denis <Deniss2007@gmail.com>
2021-11-19 18:02:24 +0200

    export

--- combineReducers/task3/src/cart.actions.js
+++ combineReducers/task3/src/cart.actions.js
@@ -1,5 +1,5 @@
-const ADD_PRODUCT = 'PRODUCTS/ADD_PRODUCT'
-const REMOVE_PRODUCT = 'PRODUCTS/REMOVE_PRODUCT'
+export const ADD_PRODUCT = 'PRODUCTS/ADD_PRODUCT'
+export const REMOVE_PRODUCT = 'PRODUCTS/REMOVE_PRODUCT'
 
 export const addProduct = (productData) => {
   return {
